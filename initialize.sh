c5d20d6d64f42a7218adf907c3ff13f323bd26b7
sgrieggs <smgrieggs@gmail.com>
2018-06-18 16:14:09 -0400

    made the scripts actually work

--- initialize.sh
+++ initialize.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-read -p "WARNING: Initialization will erase all data previously collected." -n 1 -r
+read -p "WARNING: Initialization will erase all data previously collected, are you sure you want to do this [y/n]: " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   docker-compose up -d
   sleep 1
   docker exec -it psychometricannotator_db_1 sh -c 'mysql --password=$MYSQL_ROOT_PASSWORD documents < /test/initScript.sql'
-  docker exec -it psychometricannotator_imagehandler_1 sh -c -rm /input/*
-  docker exec -it psychometricannotator_imagehandler_1 sh -c -rm /output/*
+  docker exec -it psychometricannotator_imagehandler_1 sh -c '. ~/anaconda2/bin/activate image_handler && python /app/document_processor.py'
+  docker exec -it psychometricannotator_imagehandler_1 sh -c 'rm -rf /input/*'
+  docker exec -it psychometricannotator_imagehandler_1 sh -c 'rm -rf /output/*'
 fi
