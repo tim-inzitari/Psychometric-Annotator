6979285f4750e44e792734a4264334fe82552ee8
tim-inzitari <tsinzitari@gmail.com>
2022-02-04 10:06:55 -0500

    right hebrew

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 docker-compose down --remove-orphans
 
 
-docker-compose up -d --force-recreate --renew-anon-volumes
+docker-compose up -d --force-recreate --renew-anon-volumes 
 sleep 1
-docker exec -it psychometric-annotator_db_1 sh -c 'mysql --password=$MYSQL_ROOT_PASSWORD documents < /test/initScript.sql'
+docker exec  -it psychometric-annotator_db_1 sh -c 'mysql --password=$MYSQL_ROOT_PASSWORD  documents < /test/initScript.sql'
 docker exec -it psychometric-annotator_imagehandler_1 sh -c '. ~/miniconda2/bin/activate image_handler && python /app/document_processor.py'
 docker exec -it psychometric-annotator_imagehandler_1 sh -c 'rm -rf /input/*'
 docker exec -it psychometric-annotator_imagehandler_1 sh -c 'rm -rf /output/*'
